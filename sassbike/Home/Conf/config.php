cc6b728e53cbd98a02f2772360d2240694a58afe
yingying <shy2511020145@126.com>
2017-10-23 01:56:12 +0800

    fix code v1.1

--- sassbike/Home/Conf/config.php
+++ sassbike/Home/Conf/config.php
@@ -8,7 +8,7 @@ return array(
 	TMPL_R_DELIM=>'}>',	//修改变量右定界符
 	
 	'DB_PREFIX'=>'bike_',
-	'DB_DSN'=>'mysql://root:123456@120.78.152.219:3306/sharebike', //使用DSN方式配置数据库信息,优先使用
+	'DB_DSN'=>'mysql://root:123456@localhost:3306/sharebike', //使用DSN方式配置数据库信息,优先使用
 	'SHOW_PAGE_TRACE'=>true 	//开启页面的trace
 );
 
